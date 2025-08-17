2e85f5f624a9f1aace3833623f4a095d36e6ff68
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2025-08-17 22:13:02 +0100

    feat: add global settings service with tests

---
+++ src/services/global-settings.ts
@@ -0,0 +1,31 @@
+import { ChromeStorageSync } from '../repositories';
+
+interface GetGlobalSettings {
+  accountFilterStatus?: boolean;
+  autoRefreshPage?: boolean;
+}
+
+export type GlobalSettingKey = keyof GetGlobalSettings;
+
+class GlobalSettings extends ChromeStorageSync {
+  constructor() {
+    super();
+  }
+
+  async getValue(key: GlobalSettingKey): Promise<boolean> {
+    const result = await super.get<GetGlobalSettings>(key);
+
+    if (result && key in result && typeof result[key] === 'boolean') {
+      return result[key];
+    }
+
+    await super.set({ [key]: true });
+    return true;
+  }
+
+  async setValue(key: GlobalSettingKey, value: boolean): Promise<void> {
+    return super.set({ [key]: value });
+  }
+}
+
+export const globalSettings = new GlobalSettings();
