44c753d9593a297907f1a8e17ad83c27da185d95
Aaron Kikabhai <37105197+aaronkik@users.noreply.github.com>
2025-09-06 22:38:07 +0100

    fix: global setting descriptions

--- src/popup/components/global-settings/global-setting-item.tsx
+++ src/popup/components/global-settings/global-setting-item.tsx
@@ -14,11 +14,14 @@ interface GlobalSettingItemProps {
 const GlobalSettingItem = (props: GlobalSettingItemProps) => {
   const { persistValue, persistedValue } = useGlobalSetting(props.storageKey);
 
+  const isEnabled = persistedValue;
+  const description = isEnabled ? props.description.enabled : props.description.disabled;
+
   return (
     <div className='flex flex-row items-center justify-between' key={props.title}>
       <div className='flex flex-col'>
         <p className='text-base font-bold tracking-wide'>{props.title}</p>
-        <p className='text-sm opacity-50'>{props.description.disabled}</p>
+        <p className='text-sm opacity-85'>{description}</p>
       </div>
       <Switch checked={persistedValue} onChange={persistValue} />
     </div>
